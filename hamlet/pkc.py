935aca5fc0d8b9f68ad300b066914190b46332f1
Bruno Luiz Bender Loss <61715805+N1ghtmar@users.noreply.github.com>
2023-07-23 13:44:23 -0300

    Update pkc.py
    add "ç" char

--- hamlet/pkc.py
+++ hamlet/pkc.py
@@ -16,7 +16,7 @@ class PKC:  # define a instâcia da classe
         seed(self.h_key)    # ele usa a H-KEY para definir uma seed para toda thread do código
                             # assim, sempre que você usar a seed definida,
                             # vai obter o mesmo resultado pelos metodos da biblioteca random
-        self.e_path = list(printable[:-5]+'áéóíúÁÉÓÍÚàèòìùÀÒÈÌÙôêâîûÔÂÎÛÊãõÃÕ')  
+        self.e_path = list(printable[:-5]+'áéóíúÁÉÓÍÚàèòìùÀÒÈÌÙôêâîûÔÂÎÛÊãõÃÕçÇ')  
                                             # aqui ele pega todos os caracteres printavéis do python e 
         shuffle(self.e_path)                # cria uma lista embaralhando ele
                                             # basicamente o que fazemos a seguir é só uma simples
